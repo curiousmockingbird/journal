74b1a4618fb16b944cf82977d8821e4f1726f0a7
Harold_Mesa <harold.mesa@icloud.com>
2022-08-29 19:05:05 -0500

    Add noConst() and noVowels() to Entry()
    Co-authored-by: Ted Gold <ted.jonathan.gold@gmail.com>

--- src/entry.js
+++ src/entry.js
@@ -1,5 +1,7 @@
 export default function Entry(text) {
   this.entry = text;
+  this.entryArray = [this.entry]; //"just" "the" "words"
+  this.lettersArray = []; //"j", "u", "s", "t" ...
 }
 
 Entry.prototype.wordCounter = function() {
@@ -10,6 +12,34 @@ Entry.prototype.wordCounter = function() {
     console.log(counter);
   });
   console.log(toCount);
+  this.entryArray = toCount;
   return counter;
 };
 
+// Entry.prototype.noPunctuation = function() {
+//   let noSpecial = this.entryArray.map(x=>x.replace( /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g, '' ));
+//   return noSpecial;
+// };
+
+Entry.prototype.noConst = function() {
+  let vowels = this.entryArray.map(x=>x.replace( /[qwrtypsdfghjklzxcvbnm!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g, '' ));
+  console.log(vowels);
+  return vowels;
+};
+
+Entry.prototype.noVowel = function() {
+  let consts = this.entryArray.map(x=>x.replace( /[aeiou!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g, '' ));
+  console.log(consts);
+  return consts;
+};
+
+// let combinedString = this.entry.split(" ");
+// console.log(combinedString);
+
+// for (let i = 0; i <= combinedString.length -1; i++) {
+// if (combinedString.toString().includes("e" || "a" || "i" || "o" || "u")) {
+//  combinedString.replace(/[aeiou ]/g, '');
+// console.log("The text includes vowels");
+// } 
+// }
+// return combinedString;
